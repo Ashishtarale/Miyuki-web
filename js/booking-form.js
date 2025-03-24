fd919354f953021c03da976cf5dcb4a036a4b856
Ashishtarale <124427405+Ashishtarale@users.noreply.github.com>
2025-03-24 15:55:44 +0530

    Update booking-form.js

--- js/booking-form.js
+++ js/booking-form.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loadingMessage.innerHTML = "Loading...";
 
     fetch("php/bookingForm.php", {
-      method: "POST",
+      method: "push",
       body: formData,
     })
       .then((response) => response.text())
