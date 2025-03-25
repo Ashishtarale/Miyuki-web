4128e342c650445048dd6bd3eb3774932a57180f
Ashishtarale <124427405+Ashishtarale@users.noreply.github.com>
2025-03-25 11:36:18 +0530

    Update process_booking.php

--- process_booking.php
+++ process_booking.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'ashishtarale002@gmail.com'; // Your Gmail
-        $mail->Password = 'Ashi@2000'; // Use App Password
+        $mail->Password = '#Ashi@2000!'; // Use App Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
